994ca7c91cb6f91586e16485c1499b19ba1c8fb3
Aidan Maldonado <aidanm1198@gmail.com>
2020-12-24 22:53:03 -0500

    Update
    Added more nav options

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 //Dom variables
 const startSection = document.querySelector(".start");
 const countrySection = document.querySelector(".country");
+const gameSection = document.querySelector(".game");
 const startBtn = document.querySelector("#start-btn");
-const faction1 = document.querySelector("#faction1");
-const faction2 = document.querySelector("#faction2");
-const faction3 = document.querySelector("#faction3");
+const card1 = document.querySelector("#faction1");
+const card2 = document.querySelector("#faction2");
+const card3 = document.querySelector("#faction3");
+const storyP = document.querySelector("#story-p");
+const hud = document.querySelector("#hud");
+//Slider buttons
+const rightArrow = document.querySelector("#arrow-right");
+//Game buttons
+const map = document.querySelector("#map");
+
+//Game variables
+let card = 1;
+let sCounter = 0;
 
 //Functions
 const atStart = () => {
   countrySection.style.display = "none";
+  gameSection.style.display = "none";
+  hud.style.display = "none";
+};
+
+//Picks faction
+const cardPicker = () => {
+  card1.style.display = "flex";
+  card2.style.display = "none";
+  card3.style.display = "none";
+};
+
+const storyCounter = () => {
+  sCounter += 1;
+  //Change back to == 10 after testing is done
+  if (sCounter == 2) {
+    storyP.style.display = "none";
+    hud.style.display = "flex";
+  }
+};
+
+const pickedFaction = () => {
+  countrySection.style.display = "none";
+  gameSection.style.display = "flex";
 };
 
 //Event listeners
@@ -17,5 +51,46 @@ startBtn.addEventListener("click", () => {
   countrySection.style.display = "flex";
 });
 
+//Moves the faction choices
+rightArrow.addEventListener("click", () => {
+  if (card == 1) {
+    card1.style.display = "none";
+    card2.style.display = "flex";
+    card3.style.display = "none";
+    card++;
+  } else if (card == 2) {
+    card1.style.display = "none";
+    card2.style.display = "none";
+    card3.style.display = "flex";
+    card = 0;
+  } else {
+    card1.style.display = "flex";
+    card2.style.display = "none";
+    card3.style.display = "none";
+    card++;
+  }
+});
+
+//Picks faction
+card1.addEventListener("click", () => {
+  pickedFaction();
+});
+
+card2.addEventListener("click", () => {
+  pickedFaction();
+});
+
+card3.addEventListener("click", () => {
+  pickedFaction();
+});
+
+map.addEventListener("click", () => {});
+
 //Function calls
 atStart();
+cardPicker();
+
+//Game loop
+window.setInterval(() => {
+  storyCounter();
+}, 1000);
